488be578d0ed9a012e273ae9ee25b94660168c58
JosephPark <human1654@gmail.com>
2024-03-19 02:18:28 +0900

    Improved UI & Enabled export parameter modification

--- BatchExportKraToPNG/BatchExportKraToPNG.py
+++ BatchExportKraToPNG/BatchExportKraToPNG.py
@@ -1,18 +1,10 @@
 from krita import *
 from PyQt5.QtWidgets import (
     QApplication, 
-    QMainWindow,
     QAction,
-    QCheckBox,
-    QLabel,
-    QListWidget,
-    QVBoxLayout,
-    QWidget,
-    QFormLayout,
-    QPushButton,
-    QMessageBox,
     QFileDialog
 )
+from . import UILayer
 from PyQt5.QtGui import QImage
 import os
 
@@ -24,17 +16,17 @@ class BatchExportKraToPNG(Extension):
         pass
 
     def query_directory_path(self):
-        self.src_folder.setText(QFileDialog.getExistingDirectory(None, i18n("Select a folder containing .kra files"), "", QFileDialog.ShowDirsOnly))
-        for file in os.listdir(self.src_folder.text()):
+        self.ui_layer.src_folder.setText(QFileDialog.getExistingDirectory(None, i18n("Select a folder containing .kra files"), "", QFileDialog.ShowDirsOnly))
+        for file in os.listdir(self.ui_layer.src_folder.text()):
             filename = os.fsdecode(file)
             if filename.endswith(".kra") and not filename in self.directoryList: 
                 self.directoryList.append(filename)
-                self.document_layout.addWidget(QLabel(filename))
+                self.ui_layer.document_layout.addWidget(QLabel(filename))
             else:
                 continue
 
     def set_destination_Folder(self):
-        self.dest_folder.setText(QFileDialog.getExistingDirectory(None, i18n("Select a folder .png files will be saved"), "", QFileDialog.ShowDirsOnly))
+        self.ui_layer.dest_folder.setText(QFileDialog.getExistingDirectory(None, i18n("Select a folder .png files will be saved"), "", QFileDialog.ShowDirsOnly))
 
     def warning_src_folder_null(self):
         src_null_warning = QMessageBox()
@@ -55,7 +47,7 @@ class BatchExportKraToPNG(Extension):
     def message_conversion_confirm(self):
         progress_message = QMessageBox()
         progress_message.setWindowTitle('Confirm')
-        progress_message.setText("Files will be copied to .png files. Continue?");
+        progress_message.setText("Files will be exported to .png files. Continue?");
         progress_message.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
         progress_message.setIcon(QMessageBox.Information)
         return_val = progress_message.exec()
@@ -66,25 +58,53 @@ class BatchExportKraToPNG(Extension):
     def message_conversion_finish(self):
         done_message = QMessageBox()
         done_message.setWindowTitle('Conversion is done!')
-        done_message.setText("Files were saved to \n" + self.dest_folder.text());
+        done_message.setText("Files were saved to \n" + self.ui_layer.dest_folder.text());
         done_message.setStandardButtons(QMessageBox.Close)
         done_message.setIcon(QMessageBox.Information)
         done_message.exec()
 
+    """
+    compression: int (1 to 9)
+    indexed: bool (True or False)
+    interlaced: bool (True or False)
+    saveSRGBProfile: bool (True or False)
+    forceSRGB: bool (True or False)
+    alpha: bool (True or False)
+    transparencyFillcolor: rgb (Ex:[255,255,255])
+    """
+    def get_export_parameters(self):
+        exportParameters = InfoObject()
+        compression_int = self.ui_layer.compression_slider.value
+        indexed_bool = self.ui_layer.indexed_checkbox.isChecked()
+        interlaced_bool = self.ui_layer.interlaced_checkbox.isChecked()
+        saveSRGBProfile_bool = self.ui_layer.saveSRGBProfile_checkbox.isChecked()
+        forceSRGB_bool = self.ui_layer.forceSRGB_checkbox.isChecked()
+        alpha_bool = self.ui_layer.alpha_checkbox.isChecked()
+        transpFillcolor_color = self.ui_layer.transpFillcolor
+
+        exportParameters.setProperty("compression", compression_int)
+        exportParameters.setProperty("indexed", indexed_bool)
+        exportParameters.setProperty("interlaced", interlaced_bool)
+        exportParameters.setProperty("saveSRGBProfile", saveSRGBProfile_bool)
+        exportParameters.setProperty("forceSRGB", forceSRGB_bool)
+        exportParameters.setProperty("alpha", alpha_bool)
+        exportParameters.setProperty("transparencyFillcolor", transpFillcolor_color)
+
+        return exportParameters
+
     def convert_kra_to_png(self):
+        exportParameters = self.get_export_parameters()
         for filename in self.directoryList:
-            currentDocument = Krita.instance().openDocument(self.src_folder.text() + "/" + filename)
+            currentDocument = Krita.instance().openDocument(self.ui_layer.src_folder.text() + "/" + filename)
             currentDocument.setBatchmode(True)
-            exportParameters = InfoObject()
-            exportParameters.setProperty("compression", 9) # 0-9
-            save_directory = self.dest_folder.text() + "/" + filename.rstrip(".kra") + ".png"
+            save_directory = self.ui_layer.dest_folder.text() + "/" + filename.rstrip(".kra") + ".png"
             currentDocument.exportImage(save_directory, exportParameters)
 
     def batch_save_kra_files(self):
-        if(self.src_folder.text() == ""):
+        if(self.ui_layer.src_folder.text() == ""):
             self.warning_src_folder_null()
             return
-        if(self.dest_folder.text() == ""):
+        if(self.ui_layer.dest_folder.text() == ""):
             self.warning_dest_folder_null()
             return
         if self.message_conversion_confirm():
@@ -93,30 +113,12 @@ class BatchExportKraToPNG(Extension):
 
     def initialize(self):
         self.directoryList = []
+        self.ui_layer = UILayer.UILayer()
+        self.ui_layer.initialize()
 
-        self.src_folder = QLabel()
-        self.dest_folder = QLabel()
-        self.form_layout = QFormLayout()
-        self.document_layout = QVBoxLayout()
-        self.widget_documents = QListWidget()
-
-        self.browse_src_button = QPushButton("Source Folder")
-        self.browse_dest_button = QPushButton("Destination Folder")
-        self.save_button = QPushButton("Start Batch Save")
-
-        self.browse_src_button.clicked.connect(self.query_directory_path)
-        self.browse_dest_button.clicked.connect(self.set_destination_Folder)
-        self.save_button.clicked.connect(self.batch_save_kra_files)
-
-        self.form_layout.addRow("Documents : ", self.document_layout)
-        self.form_layout.addRow("Source Folder : ", self.src_folder)
-        self.form_layout.addRow("Destination Folder : ", self.dest_folder)
-        self.form_layout.addRow(self.browse_src_button)
-        self.form_layout.addRow(self.browse_dest_button)
-        self.form_layout.addRow(self.save_button)
-        self.widget_documents.setLayout(self.form_layout)
-
-        self.widget_documents.show()
+        self.ui_layer.browse_src_button.clicked.connect(self.query_directory_path)
+        self.ui_layer.browse_dest_button.clicked.connect(self.set_destination_Folder)
+        self.ui_layer.save_button.clicked.connect(self.batch_save_kra_files)
 
     def createActions(self, window):
         action = window.createAction("", "Batch Export .kra to .png", "tools/scripts")
